91953b91ddd72bdb995b59c34b2ca262acf7a965
BJNick <nikitashets28@gmail.com>
2021-02-01 09:15:39 -0600

    Handin.py formatted

--- handin.py
+++ handin.py
@@ -14,7 +14,8 @@ import sys # Console arguments
 klembord.init()
 
 link_start = "https://github.com/BJNick/APCS-Practice/tree/master/"
-raw_link_start = "https://raw.githubusercontent.com/BJNick/APCS-Practice/master/"
+raw_link_start = \
+    "https://raw.githubusercontent.com/BJNick/APCS-Practice/master/"
 
 
 onlyfolders = [f for f in listdir("./") if (not isfile(join("./", f)) and
@@ -54,35 +55,41 @@ lower_first = lambda test_str: test_str[:1].lower() + \
 
 for i, f in enumerate(files):
     f = f.strip()
-    explanation = "- a supplementary file"
+    explanation = "is a supplementary file"
     if (f.endswith(".py")):
-        python_module = importlib.import_module(unit.replace("/", ".")+"."+f.replace(".py", ""))
+        python_module = importlib.import_module(unit.replace("/", ".") + "."
+                                                + f.replace(".py", ""))
         explanation = python_module.__doc__.splitlines()[3].strip()
-    files[i] = '<li><a href="' + link_start + unit + "/" + f + '">' + f + '</a>  ' \
-        + lower_first(explanation) + '</li>'
+
+    files[i] = '<li><a href="%s/%s">%s</a> %s</li>' \
+               % (link_start + unit, f, f, lower_first(explanation))
     if f.endswith(".png") or f.endswith(".jpg"):
-        files[i] = files[i] + '<img style="max-width: 75%; max-height: 75%; border: 1px solid; margin: auto;" src="' + raw_link_start + unit + "/" + f +'" alt="' + f + '">'
+        files[i] = files[i] + ('<img style="max-width: 75%%; max-height: 75%%;'
+       'border: 1px solid; margin: auto;" src="%s/%s" alt="%s">') \
+       % (raw_link_start + unit, f, f)
 
 
 
-greeting = "Good morning" if datetime.datetime.now().hour < 12 else "Good afternoon" if datetime.datetime.now().hour < 17 else "Good evening"
+greeting = "Good morning" if datetime.datetime.now().hour < 12 else \
+    "Good afternoon" if datetime.datetime.now().hour < 17 else "Good evening"
 
 pretty_unit = unit.split("les")
-pretty_unit = pretty_unit[0].replace("unit", "Unit ").replace("_","") + " " + pretty_unit[1].replace("_", "-").replace("son", "Lessons ").replace("/hw", " homework")
+pretty_unit = pretty_unit[0].replace("unit", "Unit ").replace("_","") + " " \
+              + pretty_unit[1].replace("_", "-").replace("son", "Lessons ") \
+                  .replace("/hw", " homework")
 
-email = greeting + " Mr. Ruo,<br><br>I have completed " + pretty_unit + ". Here are the files:<br>"
+email = "%s Mr. Ruo,<br><br>I have completed %s. " \
+        "Here are the files:<br>" % (greeting, pretty_unit)
 
 email = email + '<ul style="padding-left: 20px;">'
-
 for f in files:
     email = email + f
-
 email = email + '</ul>'
 
 email = email + "<br>Mykyta S."
 
-email = email + "<br><br>P.S. This entire email was automatically generated with "\
-                '<a href="' + link_start + "handin.py" + '">' + "handin.py" + '</a>'
+email = email + '<br><br>P.S. This entire email was automatically generated' \
+                'with <a href="%shandin.py">handin.py</a>' % (link_start)
 
 print()
 
